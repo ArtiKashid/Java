6c786132f717b2dbd272c4d125451a667f13ca77
ArtiKashid <98664015+ArtiKashid@users.noreply.github.com>
2022-09-16 13:58:01 +0530

    Square Pattern *

---
+++ SquarePattern.java
@@ -0,0 +1,26 @@
+/*Square Pattern
+ 
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
+ */
+
+package ForLoopPatterns;
+
+public class SquarePattern {
+public static void main(String arg[])
+{
+	int i,j,m=4,n=4;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			System.out.print("*"+ " ");
+		}
+		System.out.println();
+	}
+	
+}
+}
