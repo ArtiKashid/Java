7ed6071f50a62d7d078b05e17d1f4a3286232d14
ArtiKashid <98664015+ArtiKashid@users.noreply.github.com>
2022-09-18 16:15:18 +0530

    Program to print Tables from 1 to 100 For loop

---
+++ Table1To100.java
@@ -0,0 +1,14 @@
+package forLoop;
+import java.util.*;
+public class Table1To100 {
+public static void main(String arg[])
+{
+	for(int i=1;i<=10;i++)
+	{
+		for(int j=1;j<=100;j++)
+		{
+			System.out.printf("%4d",(i*j));
+		}System.out.println("\n ");
+	}System.out.println(" ");
+}
+}
